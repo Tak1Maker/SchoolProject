e13fef2fea8d4364752393e3d945479a05b02f1d
mou-inoks <mou@inokscapital.com>
2022-05-28 22:22:58 +0200

    Lots of changes

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css';
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./Components/theme";
+import Accueil from './Components/Accueil';
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+function App() {
+
+    const [theme, setTheme] = useState("light");
+
+    const themeToggler = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    };
+    return (
+        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+            <GlobalStyles />
+            <StyledApp>
+                <div className='FirstPart'>
+                    <Accueil/>
+                    <div className="toggleWrapper">
+                        <input onClick={() => themeToggler()} type="checkbox" class="dn" id="dn" />
+                        <label for="dn" class="toggle">
+                            <span class="toggle__handler">
+                                <span className="crater crater--1"></span>
+                                <span className="crater crater--2"></span>
+                                <span className="crater crater--3"></span>
+                            </span>
+                            <span className="star star--1"></span>
+                            <span className="star star--2"></span>
+                            <span className="star star--3"></span>
+                            <span className="star star--4"></span>
+                            <span className="star star--5"></span>
+                            <span className="star star--6"></span>
+                        </label>
+                    </div>
+                </div>
+                
+            </StyledApp>
+
+        </ThemeProvider>
+    );
+}
+
+export default App;
--- src/Components/Accueil.jsx
+++ src/Components/Accueil.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import video from "../Video/video.mp4"
+import BackgroundVideo from '../Video/video.mp4'
 import './Accueil.css'
+import Typewriter from 'typewriter-effect'
 
 export default function Accueil() {
   return (
-    <video id='video' src={video} autoPlay loop muted />
+      <>
+      <h1>Hello welcome to</h1>
+      <div id='TypeWriter'>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+              .typeString("GrandeMotors")
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString("Le Boss")
+              .start();
+              
+            }}/>
+        <video id='video' src={BackgroundVideo} autoPlay loop muted />
+      </div>
+
+      </>
+    
   )
 }
